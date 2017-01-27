9afc0e935e2ffe99b83ef2dccc8edf280d3ccb00
rotom <rotom@alum.mit.edu>
2017-01-26 21:12:19 -0800

    circumventing a contestant-finding bug

--- jarchive_data_scraper.py
+++ jarchive_data_scraper.py
@@ -14,20 +14,48 @@ class jeopardyGame():
         self.soup = bs4.BeautifulSoup(self.req.text, 'lxml')
 
     def get_clues_with_cats(self):
+        
         pass
 
     def get_contestants(self, gender=False):
-        return [p.contents[0].contents[0] for p in self.soup.find_all('p', class_='contestants')]
+
+        contestants = self.soup.find_all('p', class_='contestants')[:3]
+        return [c.contents[0].getText() for c in contestants]
+        
+        #return [p.contents[0].contents[0] for p in contestants]
 
     def get_date(self):
-        return self.soup.title.text.split(' ')[-1]
+        
+        date = self.soup.title.text.split(' ')[-1]
+        year, month, day = date.split('-')
+        return date
 
     def get_winner(self):
         pass
 
     def close(self):
         pass
+
+class Contestant():
+
+    def __init__(self, name, is_champ = False, winnings=0, streak=0):
         
+        self.name = name
+        self.is_champ = is_champ
+        self.winnings = winnings
+        self.streak = 0 #number of games won
+
+    def get_gender(self):
+
+        self.gender = get_gender_from_name(self.name)
+
+class Clue():
+
+    def __init__(self):
+
+        self.text = text
+        self.value = value
+        self.category = category       
             
 
 if __name__ == '__main__':
@@ -35,7 +63,7 @@ if __name__ == '__main__':
     for game_id in range(1,500):
         game_id_str = str(game_id)
         game = jeopardyGame(game_id_str)
-        #print "Game", game_id_str,":", game.get_contestants()
+        print "Game", game_id_str,":", game.get_contestants()
         game.get_date()
         game.close()
         game_id += 1
